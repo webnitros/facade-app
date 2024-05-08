a4b2b8bc5055d19936c88d3e23d7925739db52cc
Андрей <info@bustep.ru>
2024-05-08 12:54:16 +0700

    Facade readme

--- Extras/FacadeApp/core/components/FacadeApp/elements/plugins/FacadeApp.php
+++ Extras/FacadeApp/core/components/FacadeApp/elements/plugins/FacadeApp.php
@@ -1,13 +1,56 @@
 <?php
+
+use Illuminate\Support\Facades\Facade;
+use function FacadeApp\app;
+
 /** @var modX $modx */
 /* @var array $scriptProperties */
 switch ($modx->event->name) {
     case 'OnMODXInit':
-        /* @var FacadeApp $FacadeApp */
-        $FacadeApp = $modx->getService('FacadeApp', 'FacadeApp', $modx->getOption('FacadeApp_core_path', $scriptProperties, $modx->getOption('core_path') . 'components/FacadeApp/') . 'model/');
-        if ($FacadeApp instanceof FacadeApp) {
-            $FacadeApp->loadHandlerEvent($modx->event);
+
+
+        $data = $modx->getVersionData();
+
+        if ($data['version'] == 2) {
+            // Подключаем автозагрузчик только для версии MODX 2.0 и выше
+            include_once MODX_CORE_PATH . 'components/FacadeApp/vendor/autoload.php';
+
+            $app = app();
+            $app->singleton('modx', function () {
+                return $this->modx;
+            });
+
+            /*
+             * Event reg for FacadeAppAddSingleton
+             * $app->singleton('myapp', function () use ($modx) {
+             *      return $modx->getService('myapp');
+             * });
+             * */
+            $modx->invokeEvent('FacadeAppAddSingleton', [
+                'app' => $app
+            ]);
+
+            Facade::clearResolvedInstances();
+            Facade::setFacadeApplication($app);
+
+        } else {
+
+            /*
+             * From MODX 3.0 add FacadeAppAddSingleton event
+             $app->add('myapp', function () use ($modx) {
+                return  $modx->services->get('myapp');
+            });
+            */
+
+            $modx->invokeEvent('FacadeAppAddSingleton', [
+                'app' => $modx->services
+            ]);
+
+            Facade::clearResolvedInstances();
+            Facade::setFacadeApplication($modx->services);
+
         }
+
         break;
 }
 return '';
